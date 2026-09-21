23558f85abc925058e7f128449fde6be4d7b85cc
DiyaAbuZanouneh <diya.abozanouneh@gmail.com>
2026-09-21 15:10:03 +0300

    Complete the Task1

---
+++ Mon-21-9-2026/js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// Task 1
+console.log("The Type Of 100 : ", typeof 100);
+console.log("The Type Of 73.9 : ", typeof 79.3);
+console.log("The Type Of NaN : ", typeof NaN);
+console.log('The Type Of "What" : ', typeof "Water");
+console.log("The Type Of false  ", typeof (9 != 11));
+console.log('The Type Of "Orang" + "e" : ', typeof ("Orang" + "e"));
+console.log('The Type Of "Orange" - "S" : ', typeof ("Orange" - "s"));
+console.log('The Type Of "4" + "8" : ', typeof ("4" + "8"));
+console.log('The Type Of "4" - "8" : ', typeof ("4" - "8"));
+console.log('The Type Of "name" + 3 : ', typeof ("name" + 3));
+console.log('The Type Of  82 * "word" : ', typeof (82 * "word"));
+console.log('The Type Of  1 + "hello" : ', typeof (1 + "hello"));
+console.log('The Type Of  "hello" + 1 : ', typeof ("hello" + 1));
+console.log("The Type OF 1 + true : ", typeof (1 + true));
+console.log('The Type Of "hello" + true : ', typeof ("hello" + true));
+console.log("The Type Of Infinity : ", typeof Infinity);
+console.log("The Type Of  1 == '1' : ", typeof (1 == "1"));
+console.log("The Type Of 1 === '1' : ", typeof (1 === "1"));
+
+
