9680b2c56e5181857422422895cafef34804679d
DiyaAbuZanouneh <diya.abozanouneh@gmail.com>
2026-09-21 15:50:32 +0300

    Complete The Data Types and Operators Task

--- Mon-21-9-2026/js/script.js
+++ Mon-21-9-2026/js/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// Task 1
+// Class Room Task :
 console.log("The Type Of 100 : ", typeof 100);
 console.log("The Type Of 73.9 : ", typeof 79.3);
 console.log("The Type Of NaN : ", typeof NaN);
@@ -19,5 +19,57 @@ console.log('The Type Of "hello" + true : ', typeof ("hello" + true));
 console.log("The Type Of Infinity : ", typeof Infinity);
 console.log("The Type Of  1 == '1' : ", typeof (1 == "1"));
 console.log("The Type Of 1 === '1' : ", typeof (1 === "1"));
+
+// space in console window 
+console.log();
+console.log();
+
+
+// Data Types and Operators Task :
+// Cash flow ratio  =>
+let cash = 1000;
+let liabilities = 500;
+console.log(cash/liabilities);
+
+// Net income =>
+let revenues = 1000; 
+let expenses = 500;
+console.log(revenues - expenses);
+
+// Total assets  => 
+let assets_liabilities = 1000;
+let equity = 500;
+console.log(assets_liabilities + equity);
+
+// Net income (using profit margin and sales)  =>
+let profit = 1000; 
+let sales = 500; 
+console.log(profit * sales);
+
+// Average  => 
+let number1 = 7;
+let number2 = 9;
+let number3 = 2;
+console.log((number1 + number2 + number3) / 3);
+
+// Discount => 
+let price = 150;
+let discount = 0.3;
+console.log(price - (price * discount));
+
+// Age limit (older than 18 and less than 30) =>
+let age = 20;
+console.log( age > 18 && age < 30 ? true : false);
+
+// Remainder => 
+let number4 = 10;
+let number5 = 4;
+console.log(number4 % number5);
+
+
+
+
+
+
 
 
